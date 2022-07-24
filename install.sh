a85e0f6e99b62dcccd386d4f324c92f2be091d61
Finn <67797739+finnrr@users.noreply.github.com>
2022-07-23 22:47:05 -0400

    add more build more

--- install.sh
+++ install.sh
@@ -27,14 +27,16 @@ btrfs subvolume create /mnt/root
 btrfs subvolume create /mnt/home
 btrfs subvolume create /mnt/swap
 btrfs subvolume create /mnt/snaps
-# btrfs subvolume list /mnt
+btrfs subvolume list /mnt
 umount -R /mnt
+
 # remount with flags
 mount -o noatime,nodiratime,compress=zstd,space_cache=v2,ssd,subvol=root /dev/sda2 /mnt
 mkdir -p /mnt/{boot,home,swap}
 mount -o noatime,nodiratime,compress=zstd,space_cache=v2,ssd,subvol=home /dev/sda2 /mnt/home
 mount -o noatime,nodiratime,compress=no,space_cache=v2,ssd,subvol=swap /dev/sda2 /mnt/swap
 mount /dev/sda1 /mnt/boot
+
 # have a looksy
 findmnt -nt btrfs
 lsblk
@@ -45,6 +47,7 @@ dd if=/dev/zero of=/mnt/swap/swapfile bs=1M count=8196 status=progress
 chmod 0600 /mnt/swap/swapfile
 mkswap -U clear /mnt/swap/swapfile
 swapon /mnt/swap/swapfile
+
 # check it
 btrfs subvolume list -p -t /mnt
 
