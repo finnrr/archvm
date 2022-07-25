a97a541652a6dd51dc20c85efdedc0ec4e4f5f71
Finn <67797739+finnrr@users.noreply.github.com>
2022-07-24 22:38:16 -0400

    add more build more

--- install.sh
+++ install.sh
@@ -3,6 +3,9 @@
 
 # make font big
 # setfont latarcyrheb-sun32
+# or
+# pacman -S terminus-font
+# setfont ter-v32b
 
 # check internet
 # ping 8.8.8.8 -c 1
@@ -14,19 +17,36 @@ pacman -Syy
 # get time
 timedatectl set-ntp true
 
-# partition 100MB boot / rest, you'll need more for multiboot or encryption
+# partition 512MB boot / rest, you'll need more for multiboot or encryption
+wipefs -a /dev/sda
 sgdisk --zap-all /dev/sda
-sgdisk -n 0:0:+100MiB -t 0:ef00 -c 0:efi /dev/sda
-sgdisk -n 0:0:0 -t 0:8300 -c 0:root /dev/sda
+sgdisk -n 0:0:+128MiB -a 4096 -t 0:ef00 -c 0:efi /dev/sda
+# sgdisk -n 0:0:0 -t 0:8300 -c 0:root /dev/sda
+
+# 
+end_position=$(sgdisk -E /dev/sda)
+sgdisk -a 4096 -n2:0:$(( $end_position - ($end_position + 1) % 4096 )) -t 0:8300 /dev/sda
+
+
+# set up encrypted drive / use below for batch install
+# echo "password" | cryptsetup -q luksFormat /dev/sda2
+cryptsetup luksFormat -qyv --iter-time 500 --key-size 256 --sector-size 4096 --type luks2 /dev/sda2
+
+# open encrypted drive / use below for batch install / drive1 can be renamed
+# echo "password" | cryptsetup open /dev/sda2 drive1
+cryptsetup open /dev/sda2 drive1
 
 # format
 mkfs.vfat -F32 -n EFI /dev/sda1
-mkfs.btrfs -L ROOT /dev/sda2 -f
+# mkfs.btrfs -L ROOT /dev/sda2 -f
+mkfs.btrfs -L ROOT /dev/mapper/drive1
 
 # mount and make subvolumes
-mount /dev/sda2 /mnt
+# mount /dev/sda2 /mnt
+mount /dev/mapper/drive1 /mnt
 btrfs subvolume create /mnt/root
 btrfs subvolume create /mnt/home
+btrfs subvolume create /mnt/tmp
 btrfs subvolume create /mnt/log
 btrfs subvolume create /mnt/pkg
 btrfs subvolume create /mnt/swap
@@ -35,13 +55,24 @@ btrfs subvolume create /mnt/snaps
 umount -R /mnt
 
 # remount with flags
-mount -o noatime,nodiratime,compress=zstd,space_cache=v2,ssd,subvol=root /dev/sda2 /mnt
-mkdir -p /mnt/{boot,home,swap,tmp,log,pkg,.snapshots}
-mount -o noatime,nodiratime,compress=zstd,space_cache=v2,ssd,subvol=home /dev/sda2 /mnt/home
-mount -o noatime,nodiratime,compress=zstd,space_cache=v2,ssd,subvol=log /dev/sda2 /mnt/var/log
-mount -o noatime,nodiratime,compress=zstd,space_cache=v2,ssd,subvol=pkg /dev/sda2 /mnt/var/cache/pacman/pkg/
-mount -o noatime,nodiratime,compress=no,space_cache=v2,ssd,subvol=swap /dev/sda2 /mnt/swap
-mount -o noatime,nodiratime,compress=zstd,space_cache=v2,ssd,subvol=snaps /dev/sda2 /mnt/.snapshots
+# mount -o noatime,nodiratime,compress=zstd,space_cache=v2,ssd,subvol=root /dev/sda2 /mnt
+# mkdir -p /mnt/{boot,home,swap,/var/tmp,/var/log,/var/cache/pacman/pkg,.snapshots}
+# mount -o noatime,nodiratime,compress=zstd,space_cache=v2,ssd,subvol=home /dev/sda2 /mnt/home
+# mount -o noatime,nodiratime,compress=zstd,space_cache=v2,ssd,subvol=tmp /dev/sda2 /mnt/var/tmp
+# mount -o noatime,nodiratime,compress=zstd,space_cache=v2,ssd,subvol=log /dev/sda2 /mnt/var/log
+# mount -o noatime,nodiratime,compress=zstd,space_cache=v2,ssd,subvol=pkg /dev/sda2 /mnt/var/cache/pacman/pkg/
+# mount -o noatime,nodiratime,compress=zstd,space_cache=v2,ssd,subvol=snaps /dev/sda2 /mnt/.snapshots
+# mount -o noatime,nodiratime,compress=no,space_cache=v2,ssd,subvol=swap /dev/sda2 /mnt/swap
+# mount /dev/sda1 /mnt/boot
+
+mount -o noatime,nodiratime,compress=zstd,space_cache=v2,ssd,subvol=root /dev/mapper/drive1 /mnt
+mkdir -p /mnt/{boot,home,swap,/var/tmp,/var/log,/var/cache/pacman/pkg,.snapshots}
+mount -o noatime,nodiratime,compress=zstd,space_cache=v2,ssd,subvol=home /dev/mapper/drive1 /mnt/home
+mount -o noatime,nodiratime,compress=zstd,space_cache=v2,ssd,subvol=tmp /dev/mapper/drive1 /mnt/var/tmp
+mount -o noatime,nodiratime,compress=zstd,space_cache=v2,ssd,subvol=log /dev/mapper/drive1 /mnt/var/log
+mount -o noatime,nodiratime,compress=zstd,space_cache=v2,ssd,subvol=pkg /dev/mapper/drive1 /mnt/var/cache/pacman/pkg/
+mount -o noatime,nodiratime,compress=zstd,space_cache=v2,ssd,subvol=snaps /dev/mapper/drive1 /mnt/.snapshots
+mount -o noatime,nodiratime,compress=no,space_cache=v2,ssd,subvol=swap /dev/mapper/drive1 /mnt/swap
 mount /dev/sda1 /mnt/boot
 
 # check it
@@ -55,13 +86,7 @@ chmod 0600 /mnt/swap/swapfile
 mkswap -U clear /mnt/swap/swapfile
 swapon /mnt/swap/swapfile
 
-# lower swap
-sysctl vm.swappiness=10
-
 # check it
 # btrfs subvolume list -p -t /mnt
-
-# generate fstab (confirm /etc/fstab swap looks like: /swap/swapfile none swap defaults 0 0)
-genfstab -L -p /mnt >> /mnt/etc/fstab
 
 # sh -c "$(curl -fsSL https://raw.githubusercontent.com/finnrr/archvm/main/install_second.sh)"
