820c190bab1499088d89973915a32ff91fc0e649
Finn <67797739+finnrr@users.noreply.github.com>
2022-07-26 21:06:54 -0400

    add more build more

--- install.sh
+++ install.sh
@@ -12,7 +12,6 @@
 # clear console
 clear
 
-
 # make font big
 # setfont latarcyrheb-sun32
 # or
@@ -50,39 +49,44 @@ fi
 # update keyring and mirrors
 # echo Updating Keyring and Mirrors
 # pacman -Syy --noconfirm archlinux-keyring reflector
-# reflector --latest 5 --sort rate --save /etc/pacman.d/mirrorlist
-
+# reflector --age 12 --latest 10 --sort rate --protocol https --save /etc/pacman.d/mirrorlist
 # get UTC time
 echo Getting Time
 timedatectl set-ntp true
 
 # partition 512MB boot / rest, you'll need more for multiboot or encryption
 echo Wiping Data From $install_drive
+cryptsetup erase $install_drive
 wipefs -a $install_drive
 sgdisk --zap-all $install_drive
+
+
+# badblocks -c 10240 -s -w -t random -v $install_drive
 echo Partitioning $install_drive
-sgdisk -n 0:0:+128MiB -a 4096 -t 0:ef00 -c 0:efi $install_drive
+sgdisk -n 0:0:+256MiB -a 4096 -t 0:ef00 -c 0:efi $install_drive
 
 # for no encryption:
 # sgdisk -n 0:0:0 -t 0:8300 -c 0:root $installdrive
 
 # make sure rest of drive is in 4096 sized blocks for encryption
 end_position=$(sgdisk -E $install_drive)
-sgdisk -a 4096 -n2:0:$(( $end_position - ($end_position + 1) % 4096 )) -t 0:8300 -c 0:root $install_drive 
+sgdisk -a 4096 -n2:0:$(( $end_position - ($end_position + 1) % 4096 )) -t 0:8309 -c 0:root $install_drive 
 
 # set up encrypted drive 
-echo -n "$drive_pass" | cryptsetup luksFormat -qyv --iter-time 500 --key-size 256 \
---sector-size 4096 --type luks2 "$install_drive"2 -d -
+echo -n ${drive_pass} | cryptsetup luksFormat -q --iter-time 500 --key-size 256 --sector-size 4096 --type luks2 "$install_drive"2 -d -
 
 # open encrypted drive
 echo Open LUKS partition
-cryptsetup open "$install_drive"2 $drive_name
+echo -n ${drive_pass} |cryptsetup open "$install_drive"2 $drive_name
 drive_path=/dev/mapper/$drive_name
+
+# remove password from env
+unset drive_pass
 
 # format 
 echo Formating File System
 mkfs.vfat -F32 -n EFI "$install_drive"1
-mkfs.btrfs -L ROOT $drive_path -f
+mkfs.btrfs --force -L ROOT $drive_path -f
 
 # mount and make subvolumes to /mnt
 echo Mounting Drives and Making Subvolumes
@@ -101,14 +105,14 @@ umount -R /mnt
 echo Mounting Subvolumes and Boot
 mount_vars="noatime,nodiratime,compress=zstd,space_cache=v2,ssd,subvol="
 mount -o "$mount_vars"root $drive_path /mnt
-mkdir -p /mnt/{boot,home,swap,/var/tmp,/var/log,/var/cache/pacman/pkg,.snapshots}
+mkdir -p /mnt/{boot/efi,home,swap,/var/tmp,/var/log,/var/cache/pacman/pkg,.snapshots}
 mount -o "$mount_vars"home $drive_path /mnt/home
 mount -o "$mount_vars"tmp $drive_path /mnt/var/tmp
 mount -o "$mount_vars"log $drive_path /mnt/var/log
 mount -o "$mount_vars"pkg $drive_path /mnt/var/cache/pacman/pkg/
 mount -o "$mount_vars"snaps $drive_path /mnt/.snapshots
 mount -o "$mount_vars"swap $drive_path /mnt/swap
-mount "$install_drive"1 /mnt/boot
+mount "$install_drive"1 /mnt/boot/efi
 
 # disable CoW
 echo turning off CoW 
@@ -121,6 +125,7 @@ chattr +C /mnt/swap
 echo Making Swap
 dd if=/dev/zero of=/mnt/swap/swapfile bs=1M count=$swap_size status=progress
 chmod 0600 /mnt/swap/swapfile
+btrfs property set /mnt/swap/swapfile compression none #maybe redundant
 mkswap -U clear /mnt/swap/swapfile
 swapon /mnt/swap/swapfile
 
