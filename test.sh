0b6abd051acf0bf74db228309e70a3518a983f8e
Finn <67797739+finnrr@users.noreply.github.com>
2022-07-29 23:43:49 -0400

    add more build more

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 test1="one"
 test2="two"
 test3="three"
-vars='$test1 $test2 $test3'
+vars='$_ $test1 $test2 $test3'
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/finnrr/archvm/main/test_recieve.sh)" $vars
 
