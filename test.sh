d06ad9bb257242e907fbd9c0fadef388fd9a9450
Finn <67797739+finnrr@users.noreply.github.com>
2022-07-29 23:19:05 -0400

    add more build more

--- test.sh
+++ test.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env -S zsh -s
-test1="it"
-test2="is"
-test3="working"
+test1="one"
+test2="two"
+test3="three"
 
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/finnrr/archvm/main/test_recieve.sh)" $test1 $test2 $test3
+sh "$(curl -fsSL https://raw.githubusercontent.com/finnrr/archvm/main/test_recieve.sh)" $test1 $test2 $test3
+
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/finnrr/archvm/main/test.sh)"
--- test_recieve.sh
+++ test_recieve.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env -S zsh -s
-echo "$1 $2 $3"
+echo 1 is $1
+echo 2 is $2
+echo 3 is $3
